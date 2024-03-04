b6fb9ead7a79563bc9e40407594d855574b8a9a3
Mohammed  EL MESBAHI <114355058+simosultan1@users.noreply.github.com>
2024-03-04 16:15:19 +0000

    Elmesbahi_API_Security_Cat_Dogs

---
+++ cats_dogs_other/api/src/tests/test_index.py
@@ -0,0 +1,26 @@
+import unittest
+
+from fastapi.testclient import TestClient
+from cats_dogs_other.api.src import index
+
+def skip_oauth():
+    return {}
+
+
+index.skip_oidc = True
+index.app.dependency_overrides[index.oauth2_scheme] = skip_oauth
+
+client = TestClient(index.app)
+
+
+class TestIndex(unittest.TestCase):
+    def test_health(self):
+        response = client.get("/health")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"status": "OK"})
+
+    def test_upload(self):
+        with open("./cats_dogs_other/api/src/tests/resources/cat.png", "rb") as file:
+            response = client.post("/upload", files={"file": ("filename", file, "image/png")})
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json()['prediction'], 'Cat')
